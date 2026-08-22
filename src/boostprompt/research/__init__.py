f6c440bcff52498d54e4ed5654bab3f33aca8e84
Airton Lira Junior <airtonlirajr@gmail.com>
2026-08-22 16:29:28 -0300

    chore: remove DuckDuckGo research adapter

--- src/boostprompt/research/__init__.py
+++ src/boostprompt/research/__init__.py
@@ -1,12 +1,10 @@
 """Adaptadores de pesquisa externa usados pelos agentes."""
 
-from .duckduckgo_mcp import DuckDuckGoMCPResearchProvider
 from .errors import ResearchUnavailableError
 from .evidence import EvidencePolicy
 from .exa import ExaResearchProvider, HttpExaClient
 
 __all__ = [
-    "DuckDuckGoMCPResearchProvider",
     "EvidencePolicy",
     "ExaResearchProvider",
     "HttpExaClient",
